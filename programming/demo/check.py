97c5689b2762588adbb4f2d8d7ad0e096ab4df18
pcs-mec <25800932+pcs-mec@users.noreply.github.com>
2023-10-23 17:33:49 +0000

    sameness is not obvious

---
+++ programming/demo/check.py
@@ -0,0 +1,49 @@
+
+def f1(a,b):
+    return a or b
+
+def f2(a,b):
+    return a or (not a and b)
+
+def f3(a,b):
+    return (not a and b ) or (a and not b) or (a and b)
+
+
+'''
+    a function of N variables will ahve a truth table with 2**N rows
+        2 variable yields 2**2 = 4 rows
+
+    Therefore, we will test our funcitons with the following boolean values:
+               a  b
+     test 1:   0  0
+     test 2:   0  1
+     test 3:   1  0
+     test 4:   1  1
+
+'''
+
+
+# test 1: 0 0
+a = False
+b = False
+assert f1(a,b) == f2(a,b) == f3(a,b), "error on 0 0"
+
+# test 2: 0 1 
+a = False
+b= True
+assert f1(a,b) == f2(a,b) == f3(a,b), "error on 0 1"
+
+# test 3: 1 0 
+a = True
+b = False
+assert f1(a,b) == f2(a,b) == f3(a,b), "error on 1 0"
+
+
+# test 4: 1 1
+a = True
+b = True
+assert f1(a,b) == f2(a,b) == f3(a,b), "error on 1 1"
+
+print("done, looks good to me")
+
+
