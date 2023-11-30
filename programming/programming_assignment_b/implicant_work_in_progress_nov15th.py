70427b801d542e2e41a295775f9a696c419bcd6a
pcs-mec <25800932+pcs-mec@users.noreply.github.com>
2023-11-30 01:50:37 +0000

    progress nov 29th

--- programming/programming_assignment_b/implicant_work_in_progress_nov15th.py
+++ programming/programming_assignment_b/implicant_work_in_progress_nov15th.py
@@ -1,7 +1,7 @@
 from icecream import ic
 
 class Implicant:
-    def __init__(self, implicant:str, covered_minterms = None ):
+    def __init__(self, implicant:str,  covered_minterms = None ):
 
         # check if implicant parameter is str
         if not isinstance(implicant, str):
@@ -11,6 +11,38 @@ class Implicant:
         self.implicant_str = implicant
 
         self.implicant = self.set_implicant()
+
+        self.implicant_bin = self.get_implicant_bin_repr()
+
+        self.cover = self.set_cover(covered_minterms)
+
+    def get_implicant_bin_repr(self):
+        
+        result = []
+        for element in self.implicant:
+            if element[-1] == "'":
+                result.append(0)
+            else:
+                result.append(1)
+        
+        # result = [ 0 if element[-1] == "'" else 1 for element in self.implicant ]
+        return tuple(result)
+
+    def set_cover(self, covered_minterms):
+        '''
+        convert self.implicant_bin to decimal and return
+
+        '''
+        if not covered_minterms:
+            result = 0
+            # exponent = 0
+            for index in range(len(self.implicant_bin)-1, -1, -1):
+                result += self.implicant_bin[index] * 2**(len(self.implicant_bin)-1 - index)
+                # exponent += 1
+            return tuple([ result]) 
+
+        return covered_minterms
+
 
     def set_implicant(self):
         index = len(self.implicant_str) -1
@@ -27,7 +59,46 @@ class Implicant:
 
     def get_implicant(self):
         return self.implicant
+    
+    def simplify(self, another_implicant):
+        if not isinstance(another_implicant, Implicant):
+            raise TypeError("another_implicant must be of Implicant class")
+
+        count_of_differences = 0
+        result = ""
+        index = 0
+
+        if len(self.implicant) != len(another_implicant.implicant):
+            return None
+        
+        # if 
+
+        while index < len(self.implicant):
+            if self.implicant[index] == another_implicant.implicant[index]:
+                result = f'{result}{self.implicant[index]}'
+                # result = result + self.implicant[index]
+            else:
+                count_of_differences += 1
+            
+            index +=1    
+        
+        if  count_of_differences != 1:
+            return None
+
+        return Implicant(result, covered_minterms = self.cover + another_implicant.cover)
+
+
 
 if __name__ == "__main__":
-    implicant1 = Implicant("a'bc'")
+    implicant1 = Implicant("a'bc")
+    implicant2 = Implicant("abc")
+    ic(implicant1.implicant_bin )
+    ic(implicant1.cover )
     ic( implicant1.get_implicant() )
+    ic( implicant2.get_implicant() )
+
+    result = implicant1.simplify(implicant2) 
+    ic( result.get_implicant() )
+    ic( result.cover )
+    
+    # --> object of implicant class, result.get() --> ("b","c'")
